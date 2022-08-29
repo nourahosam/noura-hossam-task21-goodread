48fc84fd8cf2548a5e1423c5f3d662d638c37fb8
Noura Hossam <Noura.Hossam@itworx.com>
2022-08-29 11:59:39 +0200

    Done

--- src/Components/BookDetails.js
+++ src/Components/BookDetails.js
@@ -1,29 +1,29 @@
-import React, { useEffect, useState, useContext } from 'react';
-import {Route, Link, Routes, useParams} from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Route, Link, Routes, useParams } from 'react-router-dom';
 import axios from 'axios';
-import { DataContext } from './DataContext';
-
-const BookDetails = (props) =>{
+const BookDetails = (props) => {
     const params = useParams();
     const [data, setData] = useState([]);
-    
-
-    useEffect(()=>{
-        const  books  = JSON.parse(localStorage.getItem('books')) || [];
-        const book = books.find(x=>x.id == params.id)
-        console.log("JSKENF.SDKJN.NFESDKL",book);
-        setData(book);
-
+    const [isLoading, setLoading] = useState(false);
+    useEffect(() => {
+        console.log("useEffect")
+        //console.log(params.title);
+        axios.get(`https://www.googleapis.com/books/v1/volumes/${params.id}`)
+            .then((res) => {
+                console.log("res", res.data);
+                setData(res.data.volumeInfo);
+                setLoading(true);
+            })
     }, [])
-    return(<div className='book-details-container'>
+    return (<div className='book-details-container'>
         <div className='book-details-image'>
-        <img src={data.imageLinks.thumbnail ? `${data.imageLinks.thumbnail}` : ''}/>
+            {isLoading && <img src={data.imageLinks.thumbnail} />}
         </div>
         <div className='book-details-content'>
-    <h2>{data.title}</h2>
-    <p>{data.authors} | {data.publisher}</p>
-    <p>{data.publishedDate}</p>
-    <p>{data.description}</p>
+            <h2>{isLoading && data.title}</h2>
+            <p> {isLoading && data.authors} | {isLoading && data.publisher}</p>
+            <p>{isLoading && data.publishedDate}</p>
+            <p>{isLoading && data.description}</p>
         </div>
 
     </div>)
